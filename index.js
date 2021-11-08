4f7734dae517731509515095b75ffe1add46319c
Pedro Delgado <pedro.delgado2017@mesacharter.org>
2021-11-08 15:23:37 +0000

    code challenge complete

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 //write your code here
+function averageInArray(numbers) {
+    let average = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        average += numbers[i];
+    }
+    average = average / numbers.length
+    console.log(average)
+}
+averageInArray([2,2,3,3,0])
